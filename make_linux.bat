9b834e776619f1207904bf0c2b42eda004ff13e1
root <root@konstantin-virtual-machine.(none)>
2012-03-23 01:13:44 +0400

    Adding installing on Linux.

---
+++ make_linux.bat
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+VERSION="0.8"
+DIST="SwarmVideo.linux-$VERSION.tar.gz"
+
+rm -R build
+rm -R dist
+
+python setup.py build
+
+mkdir dist
+cp LICENSE.txt dist
+cp README.linux dist
+cd build/exe.linux-i686-2.7
+tar -cvzf $DIST *
+mv $DIST ../../dist
+
+
+#HOME="/root"
+#cd /usr/bin/swarmvideo
+#./SwarmEngine &
+
+#FLAG=`cat /etc/profile | grep SwarmEngine`
+#if [ -z "$FLAG" ]
+#then
+#  echo "/usr/bin/swarmvideo/SwarmEngine" >> /etc/profile
+#fi
