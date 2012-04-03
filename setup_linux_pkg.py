e2ec86ca374dabdf28e306271e60989d51e43f40
Konstantin <konstantin@Konstantins-Mac.local>
2012-04-03 05:43:14 -0700

    Change MAC OS installator.

---
+++ setup_linux_pkg.py
@@ -0,0 +1,14 @@
+from cx_Freeze import setup, Executable
+
+includes = ["encodings.utf_8", "encodings.latin_1", "encodings.ascii", "encodings.raw_unicode_escape"]
+includefiles = [ (r"BaseLib/Images", "BaseLib/Images") ]
+setup(
+    name = "SwarmVideo",
+    version = "0.8",
+    description = "Swarm Video",
+    options = { "build_exe": { "includes": includes,
+                               "include_files": includefiles
+                             }
+    
+              },
+    executables = [Executable("BaseLib/Plugin/SwarmEngine.py")])
