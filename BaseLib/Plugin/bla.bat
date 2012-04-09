66332fb8ddd45fe86fb5a933d1d09fde063d5ea0
Anaconda84 <nemo84@mail.ru>
2012-04-09 22:41:49 +0400

    Adding bugs.

---
+++ BaseLib/Plugin/bla.bat
@@ -0,0 +1,1 @@
+"\Program Files\GnuWin32\bin\wget.exe" -S  http://127.0.0.1:6878/content/122c78693f4a376d7810dfe92b87bed6b213a984/0.925237974448.mp4
--- bla.bat
+++ bla.bat
@@ -1,1 +1,1 @@
-"\Program Files\GnuWin32\bin\wget.exe" -S  http://127.0.0.1:6878/content/4fc74e884364d1bd9155f89f0859e8ef26c74e90/0.0427276132936.mp4
+"c:\Program Files\GnuWin32\bin\wget.exe" -S  http://127.0.0.1:6878/content/122c78693f4a376d7810dfe92b87bed6b213a984/0.89452505122.mp4
