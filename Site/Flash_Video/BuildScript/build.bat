78f2abf89f100f2b7bbd69d60227bb5fef8daa44
Anaconda84 <nemo84@mail.ru>
2012-07-17 16:35:01 +0400

    New HTML5 video(VideoJS). Delete files.

--- Site/Flash_Video/BuildScript/build.bat
+++
@@ -1,5 +0,0 @@
-copy /Y "C:\Users\kos\Adobe Flash Builder 4.6\Test\src\Test.as" flex_sdk_3.6a
-del /F flex_sdk_3.6a\test.swf
-cd flex_sdk_3.6a
-start /WAIT start.bat
-copy /Y test.swf C:\nginx-1.2.0\html
